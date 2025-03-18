1bc3f75e06d6a45dfa21ddac55e8c382eb96cc9e
Robert Greathouse <robert@basistheory.com>
2025-03-18 13:12:34 -0500

    Remove v1 search test

--- tests/integration/canary.test.ts
+++ tests/integration/canary.test.ts
@@ -676,25 +676,6 @@ describe('Canary', () => {
         expect(firstTokenId).toBe(secondTokenId);
     });
 
-    it('should paginate on tokens/list v1', async () => {
-        const client = getPrivateClient();
-        const pageSize = 3;
-
-        const tokens = await client.tokens.list({
-            page: 1,
-            size: pageSize
-        });
-
-        let count = 0;
-        for await (const token of tokens) {
-            count++;
-            if (count > pageSize) {
-                break;
-            }
-        }
-        expect(count).toBeGreaterThan(pageSize);
-    });
-
     it('should paginate on tokens/list v2', async () => {
         const client = getPrivateClient();
         const pageSize = 3;
