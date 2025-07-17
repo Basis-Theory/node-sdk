b72338946e52a494c0f2af4189435d45c4543c0a
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-07-17 20:25:59 +0000

    SDK regeneration

--- src/serialization/resources/accountUpdater/resources/realTime/client/requests/AccountUpdaterRealTimeRequest.ts
+++ src/serialization/resources/accountUpdater/resources/realTime/client/requests/AccountUpdaterRealTimeRequest.ts
@@ -13,7 +13,6 @@ export const AccountUpdaterRealTimeRequest: core.serialization.Schema<
     tokenId: core.serialization.property("token_id", core.serialization.string()),
     expirationYear: core.serialization.property("expiration_year", core.serialization.number().optional()),
     expirationMonth: core.serialization.property("expiration_month", core.serialization.number().optional()),
-    deduplicateToken: core.serialization.property("deduplicate_token", core.serialization.boolean().optional()),
 });
 
 export declare namespace AccountUpdaterRealTimeRequest {
@@ -21,6 +20,5 @@ export declare namespace AccountUpdaterRealTimeRequest {
         token_id: string;
         expiration_year?: number | null;
         expiration_month?: number | null;
-        deduplicate_token?: boolean | null;
     }
 }
