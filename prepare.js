446862df2d639cc6a9168d438f7687751e157a4f
Robert Greathouse <robert@basistheory.com>
2025-08-05 08:11:24 -0500

    Remove files and keep devDependencies

--- prepare.js
+++ prepare.js
@@ -1,15 +1,9 @@
 const fs = require('fs');
 const libPackage = require('./package.json');
 
-// remove not required fields
-delete libPackage.devDependencies;
-
 libPackage.publishConfig = {
   access: 'public',
 }
-
-// include all 'dist/*' files, but bundles
-libPackage.files = ['*', '!*.bundle.js'];
 
 fs.writeFileSync(
   './package.json',
