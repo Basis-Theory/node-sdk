676e00fad1c5a3d43c141350bac46ac93a48baad
Robert Greathouse <robert@basistheory.com>
2025-08-05 08:16:06 -0500

    Better fix

--- prepare.js
+++ prepare.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const libPackage = require('./package.json');
 
+// remove not required fields
+delete libPackage.devDependencies;
+
 libPackage.publishConfig = {
   access: 'public',
 }
