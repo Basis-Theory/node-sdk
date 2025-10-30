91341b6375ebba6c0d66dfa7269f0e8c72f8897c
Kevin Peraza <kevin@basistheory.com>
2025-10-30 12:16:00 -0600

    chore: add trusted publishing (#129)
    * fix: add trusted publishing
    
    * chore: add deploy-dev to fernignore
    
    * fix: package json

--- prepare.js
+++ prepare.js
@@ -4,8 +4,11 @@ const libPackage = require('./package.json');
 // remove not required fields
 delete libPackage.devDependencies;
 
-libPackage.publishConfig = {
-  access: 'public',
+// Preserve publishConfig if it already exists, otherwise set default
+if (!libPackage.publishConfig) {
+  libPackage.publishConfig = {
+    access: 'public',
+  }
 }
 
 fs.writeFileSync(
